e616245f0cccd408ca4822349c9044ee42d608eb
Rauf Dimaampao <adimaampao@stratpoint.com>
2021-04-25 11:02:18 +0800

    revisions

--- admin/php_function/dbconn.php
+++ admin/php_function/dbconn.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbUsername = "bluegarden_db";
-$dbPassword = "bluegarden_db";
-$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbServername = "";
 $dbName = "bluegarden_db";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die("connection failed");
