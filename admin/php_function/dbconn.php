24731441671a1be27e3989394cdee904b7f1e15e
Rauf Dimaampao <adimaampao@stratpoint.com>
2021-05-07 16:20:38 +0800

    change db

--- admin/php_function/dbconn.php
+++ admin/php_function/dbconn.php
@@ -5,4 +5,12 @@ $dbPassword = "bluegarden_db";
 $dbServername = "localhost";
 $dbName = "bluegarden_db";
 
+
+
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbServername = "localhost";
+// $dbName = "bluegarden_db";
+
+
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die("connection failed");
