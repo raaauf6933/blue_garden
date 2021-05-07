47a5bd57480f95f74d372730a0dfa08fbf0a126f
Rauf Dimaampao <adimaampao@stratpoint.com>
2021-05-07 20:11:53 +0800

    fix db

--- admin/php_function/dbconn.php
+++ admin/php_function/dbconn.php
@@ -1,16 +1,16 @@
 <?php
 
-// $dbUsername = "bluegarden_db";
-// $dbPassword = "bluegarden_db";
-// $dbServername = "localhost";
-// $dbName = "bluegarden_db";
-
-
-
-$dbUsername = "root";
-$dbPassword = "";
+$dbUsername = "bluegarden_db";
+$dbPassword = "bluegarden_db";
 $dbServername = "localhost";
 $dbName = "bluegarden_db";
+
+
+
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbServername = "localhost";
+// $dbName = "bluegarden_db";
 
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die("connection failed");
