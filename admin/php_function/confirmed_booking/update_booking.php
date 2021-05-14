50aff2421b1c59b860abaee13cd243befa425bd7
Rauf Dimaampao <adimaampao@stratpoint.com>
2021-05-14 17:23:31 +0800

    fdsfs

--- admin/php_function/confirmed_booking/update_booking.php
+++ admin/php_function/confirmed_booking/update_booking.php
@@ -26,13 +26,14 @@ while ($row = mysqli_fetch_assoc($query_payment)) {
 $balance = $total_amount - $total_payed;
 
 
-if($payed_capital == $balance){
+if($payed_capital == (float)number_format($balance, 2)){
     $sql = mysqli_query($conn, "INSERT INTO payment(billing_id,payed_capital,payment_date) 
 VALUES ('$billing_id','$payed_capital','$payment_date')");
     $sql_update = mysqli_query($conn, "UPDATE reservation SET status = '6' WHERE billing_id ='$billing_id'");
 echo json_encode('1');
-}elseif(floatval($payed_capital) > floatval($balance)){
+}elseif(floatval($payed_capital) > (float)number_format($balance, 2)){
     echo json_encode('2');
 }else{
+ 
      echo json_encode('0');
 }
